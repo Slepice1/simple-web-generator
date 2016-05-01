247ee3302526b4c8a40e129a5f2b5def1d186d56
Vojtěch Jelínek <vojtin.j@gmail.com>
2016-05-01 19:21:24 +0200

    test fix and click test

--- simple_web_generator/main.py
+++ simple_web_generator/main.py
@@ -2,56 +2,16 @@
 # -*- coding: utf-8 -*-
 """Program entry point"""
 
-from __future__ import print_function
-
 import argparse
 import sys
 
-from simple_web_generator import metadata
+import click
+import yaml
 
-
-def main(argv):
-    """Program entry point.
-
-    :param argv: command-line arguments
-    :type argv: :class:`list`
-    """
-    author_strings = []
-    for name, email in zip(metadata.authors, metadata.emails):
-        author_strings.append('Author: {0} <{1}>'.format(name, email))
-
-    epilog = '''
-{project} {version}
-
-{authors}
-URL: <{url}>
-'''.format(
-        project=metadata.project,
-        version=metadata.version,
-        authors='\n'.join(author_strings),
-        url=metadata.url)
-
-    arg_parser = argparse.ArgumentParser(
-        prog=argv[0],
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        description=metadata.description,
-        epilog=epilog)
-    arg_parser.add_argument(
-        '-V', '--version',
-        action='version',
-        version='{0} {1}'.format(metadata.project, metadata.version))
-
-    arg_parser.parse_args(args=argv[1:])
-
-    print(epilog)
-
-    return 0
-
-
-def entry_point():
-    """Zero-argument entry point for use with setuptools/distribute."""
-    raise SystemExit(main(sys.argv))
-
+@click.command()
+@click.argument('input', type=click.File('r'))
+def main():
+    click.echo("lol xd")
 
 if __name__ == '__main__':
-    entry_point()
+    main()
